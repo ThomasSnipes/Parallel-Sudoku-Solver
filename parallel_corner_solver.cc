7235ea0d0bc5b2de72e3ce1497a0eaea51ff1338
Marco Clark <mac325@lehigh.edu>
2024-05-06 20:54:10 -0400

    added corner methods

--- parallel_corner_solver.cc
+++ parallel_corner_solver.cc
@@ -9,9 +9,9 @@ using namespace std;
 
 const int N = 9;
 const int SUBGRID_SIZE = 3;
-const int num_threads = 5;
-const int NUM_STARTING_CELLS = 20;
-std::atomic<int> filled_counter{};
+const int num_threads = 4;
+//const int NUM_STARTING_CELLS = 20;
+
 vector<vector<int> > board(N, vector<int>(N, 0));
 
 bool isValid(int row, int col, int num) {
@@ -20,11 +20,9 @@ bool isValid(int row, int col, int num) {
         if (board[row][i] == num || board[i][col] == num)
             return false;
     }
-    
     // Check if the number is already present in the 3x3 box
     int startRow = row - row % 3;
     int startCol = col - col % 3;
-
     // Loop over local grid
     for (int i = 0; i < SUBGRID_SIZE; ++i) {
         for (int j = 0; j < SUBGRID_SIZE; ++j) {
@@ -38,27 +36,14 @@ bool isValid(int row, int col, int num) {
 mutex mtx; //global mutex
 
 bool solveSudokuUtil(int row, int col) {
-    //At end of grid 
-    //also want to check that the grid is full
-    // if (row <=2)
-    //     std::cout << "operation on r,c: " << row << ", " << col << std::endl;
     if (row == N - 1 && col == N){
-        // if (filled_counter == (N*N)){
-        //     std::cout << "sat" << std::endl;
-        //     return true;
-        // }
-        //then we are not done i.e the grid is not full. Send the worker thread back to the start
-        // row = 0; 
-        // col = 0;
         return true;
     }
-
     // Move to the next row if the column index has reached the end
     if (col == N) {
         row++;
         col = 0;
     }
-
     //move to next cell if current is occupied
     if (board[row][col] != 0)
         return solveSudokuUtil(row, col + 1);
@@ -69,24 +54,117 @@ bool solveSudokuUtil(int row, int col) {
         if (isValid(row, col, num)) {
             // Place the number and recursively solve for the next cell
             //lock before placing
-            //scope for lock
-            {
+            {//scope for lock
                 lock_guard<mutex> lock(mtx);
                 board[row][col] = num;
-                filled_counter++;
-                //std::cout << filled_counter << std::endl;
             }
-            
-
             if (solveSudokuUtil(row, col + 1)) //increase column index here
                 return true;
-
-            //begin backtrack
-            {
+            {//placement was bad
                 lock_guard<mutex> lock(mtx);
                 board[row][col] = 0;
-                //bad choice, remove it
-                filled_counter--;
+            }
+        }
+    }
+    return false; // No solution found
+}
+
+bool solveSudokuUtil1(int row, int col) {
+    if (row == N - 1 && col == -1){
+        return true;
+    }
+    // Move to the next row if the column index has reached the end
+    if (col == -1) {
+        row++;
+        col = N-1;
+    }
+    //move to next cell if current is occupied
+    if (board[row][col] != 0)
+        return solveSudokuUtil1(row, col - 1);
+
+    //range of possible numbers is 1-9
+    for (int num = 1; num <= N; ++num) {
+        // Check if it's safe to place 'num' in the current cell
+        if (isValid(row, col, num)) {
+            // Place the number and recursively solve for the next cell
+            //lock before placing
+            {//scope for lock
+                lock_guard<mutex> lock(mtx);
+                board[row][col] = num;
+            }
+            if (solveSudokuUtil1(row, col - 1)) //increase column index here
+                return true;
+            {//placement was bad
+                lock_guard<mutex> lock(mtx);
+                board[row][col] = 0;
+            }
+        }
+    }
+    return false; // No solution found
+}
+
+bool solveSudokuUtil2(int row, int col) {
+    if (row == 0 && col == -1){
+        return true;
+    }
+    // Move to the next row if the column index has reached the end
+    if (col == -1) {
+        row--;
+        col = 0;
+    }
+    //move to next cell if current is occupied
+    if (board[row][col] != 0)
+        return solveSudokuUtil2(row, col - 1);
+
+    //range of possible numbers is 1-9
+    for (int num = 1; num <= N; ++num) {
+        // Check if it's safe to place 'num' in the current cell
+        if (isValid(row, col, num)) {
+            // Place the number and recursively solve for the next cell
+            //lock before placing
+            {//scope for lock
+                lock_guard<mutex> lock(mtx);
+                board[row][col] = num;
+            }
+            if (solveSudokuUtil2(row, col - 1)) //increase column index here
+                return true;
+            {//placement was bad
+                lock_guard<mutex> lock(mtx);
+                board[row][col] = 0;
+            }
+        }
+    }
+    return false; // No solution found
+}
+
+bool solveSudokuUtil3(int row, int col) {
+    if (row == 0 && col == N){
+        return true;
+    }
+    // Move to the next row if the column index has reached the end
+    if (col == N) {
+        row--;
+        col = 0;
+    }
+    //move to next cell if current is occupied
+    if (board[row][col] != 0)
+        return solveSudokuUtil3(row, col + 1);
+
+    //range of possible numbers is 1-9
+    for (int num = 1; num <= N; ++num) {
+        // Check if it's safe to place 'num' in the current cell
+        if (isValid(row, col, num)) {
+            // Place the number and recursively solve for the next cell
+            //lock before placing
+            {//scope for lock
+                lock_guard<mutex> lock(mtx);
+                board[row][col] = num;
+            }
+            if (solveSudokuUtil3(row, col + 1)) //increase column index here
+                return true;
+            {//placement was bad
+                lock_guard<mutex> lock(mtx);
+                board[row][col] = 0;
             }
         }
     }
@@ -103,17 +181,14 @@ bool solveSudoku() {
                     res = res | solveSudokuUtil(0, 0);
                 });
     threads.emplace_back([&](){
-                    res = res | solveSudokuUtil(0, N-1);
+                    res = res | solveSudokuUtil1(0, N-1);
                 });
     threads.emplace_back([&](){
-                    res = res | solveSudokuUtil(N-1, 0);
+                    res = res | solveSudokuUtil2(N-1, 0);
                 });
     threads.emplace_back([&](){
-                    res = res | solveSudokuUtil(N-1, N-1);
+                    res = res | solveSudokuUtil3(N-1, N-1);
                 });
-
-
-    
     for (auto &t : threads){
         t.join();
     }
@@ -141,37 +216,38 @@ void printBoard(vector<vector<int>> board) {
 }
 
 // Generate initial Sudoku board
-void initializeBoard(vector<vector<int> >& board) {
-    srand(time(0));
-    int count = NUM_STARTING_CELLS;
+// void initializeBoard(vector<vector<int> >& board) {
+//     srand(time(0));
+//     int count = NUM_STARTING_CELLS;
 
-    while (count > 0) {
-        int row = rand() % N;
-        int col = rand() % N;
-        int num = rand() % N + 1;
+//     while (count > 0) {
+//         int row = rand() % N;
+//         int col = rand() % N;
+//         int num = rand() % N + 1;
 
-        if (board[row][col] == 0 && isValid(row, col, num)) {
-            board[row][col] = num;
-            count--;
-        }
-    }
-}
+//         if (board[row][col] == 0 && isValid(row, col, num)) {
+//             board[row][col] = num;
+//             count--;
+//         }
+//     }
+// }
 
 int main() {
 
 
-    // board =  {{0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    // };
+    board = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
 
-    initializeBoard(board);
+    //initializeBoard(board);
     //printBoard(board);
     if (solveSudoku()) {
         cout << "Sudoku solved successfully:\n";
